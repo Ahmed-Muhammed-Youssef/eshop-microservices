8f36cf799738d1962fb6fe0e1b68be2cc8605022
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-12-11 21:38:19 +0200

    feat: add get products endpoint #1

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.API.Models;
+
+namespace Catalog.API.Products.GetProducts
+{
+    public record GetProductsQuery() : IQuery<GetProductsResult>;
+    public record GetProductsResult(IEnumerable<Product> products);
+    internal class GetProductsHandler(IDocumentSession documentSession, ILogger<GetProductsHandler> logger) : IQueryHandler<GetProductsQuery, GetProductsResult>
+    {
+        private readonly IDocumentSession _documentSession = documentSession;
+        private readonly ILogger<GetProductsHandler> _logger = logger;
+
+        public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation(nameof(GetProductsHandler) + " is called with query {@Query}", query);
+
+            var products = await _documentSession.Query<Product>().ToListAsync(cancellationToken);
+
+            return new GetProductsResult(products);
+        }
+    }
+}
