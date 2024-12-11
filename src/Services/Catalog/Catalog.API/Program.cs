b16b6dae5743d49b426478ba68cbc1a9c8225a61
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-12-11 13:21:34 +0200

    feat: configure api endpoint project to use carter and mediator #1

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,3 +1,16 @@
+
 var builder = WebApplication.CreateBuilder(args);
+// Add Services
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config => 
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
 var app = builder.Build();
+
+// Configure Pipeline
+
+app.MapCarter();
+
 app.Run();
