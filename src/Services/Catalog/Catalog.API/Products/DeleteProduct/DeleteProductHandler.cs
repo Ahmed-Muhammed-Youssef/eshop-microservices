f3e7bd3cf09989de50e85cb1df7a78bb0b02b89a
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-12-12 14:49:14 +0200

    feat: add delete product endpoint #1

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Catalog.API.Products.DeleteProduct
+{
+    public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+    public record DeleteProductResult(bool IsSuccess);
+    public class DeleteProductHandler(IDocumentSession documentSession, ILogger<DeleteProductHandler> logger) : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+    {
+        private readonly IDocumentSession _documentSession = documentSession;
+        private readonly ILogger<DeleteProductHandler> _logger = logger;
+
+        public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation(nameof(DeleteProductHandler) + " is called with command {@Command}", command);
+            _documentSession.Delete<Product>(command.Id);
+
+            await _documentSession.SaveChangesAsync(cancellationToken);
+
+            return new DeleteProductResult(true);
+        }
+    }
+}
