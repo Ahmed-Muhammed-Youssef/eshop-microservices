1079032f8664677f8e34e0a99670577d702e3980
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-12-12 13:32:09 +0200

    feat: add get product by category endpoint #1

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.API.Models;
+
+namespace Catalog.API.Products.GetProductByCategory
+{
+    public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+    public record GetProductByCategoryResult(IEnumerable<Product> Products);
+    internal class GetProductByCategoryHandler(IDocumentSession doumentSession, ILogger<GetProductByCategoryHandler> logger) : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+    {
+        private readonly IDocumentSession _doumentSession = doumentSession;
+        private readonly ILogger<GetProductByCategoryHandler> _logger = logger;
+
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation(nameof(GetProductByCategoryHandler) + " is called with query {@Query}", query);
+
+            IEnumerable<Product> products = await _doumentSession.Query<Product>().Where(p => p.Category.Contains(query.Category)).ToListAsync(token: cancellationToken);
+
+            return new GetProductByCategoryResult(products);
+        }
+    }
+}
