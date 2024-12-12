5eb4c7c065d653f03a057a67a6df1bbbd6536cd8
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-12-12 13:32:09 +0200

    feat: add get product by category endpoint #1

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -0,0 +1,20 @@
+﻿
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.GetProductByCategory
+{
+    public record GetProductByCategoryResponse(IEnumerable<Product> Products);
+    public class GetProductByCategoryEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/category/{categoty}", async (string categoty, ISender sender) => {
+                GetProductByCategoryResult result = await sender.Send(new GetProductByCategoryQuery(categoty));
+
+                var response = result.Adapt<GetProductByCategoryResponse>();
+
+                return Results.Ok(response);
+            });
+        }
+    }
+}
