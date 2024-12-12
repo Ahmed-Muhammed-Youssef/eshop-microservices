e98907123821c21a4458c8471e2c023d3e0a9e6c
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-12-12 14:31:46 +0200

    feat: add update product endpoint #1

---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,34 @@
+﻿namespace Catalog.API.Products.UpdateProduct
+{
+    public record UpdateProductCommand(Guid Id, string Name, List<string> Category, string ImageFile, string Description, decimal Price) : ICommand<UpdateProductResult>;
+    public record UpdateProductResult(bool IsSuccess);
+
+    internal class UpdateProductHandler(IDocumentSession documentSession, ILogger<UpdateProductHandler> logger) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+    {
+        private readonly IDocumentSession _documentSession = documentSession;
+        private readonly ILogger<UpdateProductHandler> _logger = logger;
+
+        public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation(nameof(UpdateProductHandler) + " is called with the command {@Command}", command);
+
+            var product = await _documentSession.LoadAsync<Product>(command.Id, cancellationToken);
+
+            if (product is null)
+            {
+                throw new Exception("Product not found");
+            }
+            
+            product.Name = command.Name;
+            product.Category = command.Category;
+            product.ImageFile = command.ImageFile;
+            product.Description = command.Description;
+            product.Price = command.Price;
+
+            _documentSession.Update(product);
+            await _documentSession.SaveChangesAsync(cancellationToken);
+
+            return new UpdateProductResult(true);
+        }
+    }
+}
