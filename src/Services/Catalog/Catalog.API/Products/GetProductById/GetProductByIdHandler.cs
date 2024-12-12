13da5fe835c47578589638be211c4b583cbf6d5a
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-12-12 13:35:12 +0200

    refact: move common using statement to the global usings file #1

--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -1,6 +1,4 @@
-﻿using Catalog.API.Models;
-
-namespace Catalog.API.Products.GetProductById
+﻿namespace Catalog.API.Products.GetProductById
 {
     public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
     public record GetProductByIdResult(Product Product);
