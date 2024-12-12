94720017ade544a675495323f8f161d7b064e86c
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-12-12 13:11:30 +0200

    feat: add get product by id endpoint #1

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,22 @@
+﻿using Catalog.API.Models;
+
+namespace Catalog.API.Products.GetProductById
+{
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+    public record GetProductByIdResult(Product Product);
+    public class GetProductByIdHandler(IDocumentSession documentSession, ILogger<GetProductByIdHandler> logger) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        private readonly IDocumentSession _documentSession = documentSession;
+        private readonly ILogger<GetProductByIdHandler> _logger = logger;
+
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation(nameof(GetProductByIdHandler) + " is called with query {@Query}", query);
+
+            var product = await _documentSession.LoadAsync<Product>(query.Id, cancellationToken);
+
+            return product is null ? throw new Exception("Product not found") : new GetProductByIdResult(product);
+        }
+       
+    }
+}
