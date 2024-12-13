1988e77e6b1ae9cf6010eb241b721df56b53a070
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-12-13 14:49:48 +0200

    refact: catalog api #3

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,16 +1,17 @@
 
-using BuildingBlocks.Behaviors;
-
 var builder = WebApplication.CreateBuilder(args);
 // Add Services
+var programAssemply = typeof(Program).Assembly;
+
 builder.Services.AddCarter();
+
 builder.Services.AddMediatR(config => 
 {
-    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.RegisterServicesFromAssembly(programAssemply);
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
 });
 
-builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+builder.Services.AddValidatorsFromAssembly(programAssemply);
 
 builder.Services.AddMarten(options => {
     options.Connection(builder.Configuration.GetConnectionString("PostgresConnection")!);
