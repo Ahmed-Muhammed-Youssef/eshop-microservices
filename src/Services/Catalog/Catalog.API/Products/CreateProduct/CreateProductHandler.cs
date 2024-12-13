5aaea26b0802bae0e028cb5b44b8ad09bc9e6b01
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-12-13 14:21:12 +0200

    feat: add validation for product creation #3

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,13 +1,38 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿
+
+namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler(IDocumentSession documentSession) : ICommandHandler<CreateProductCommand, CreateProductResult>
+
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(c => c.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(c => c.Category).NotEmpty().WithMessage("Category is required");
+            RuleFor(c => c.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+            RuleFor(c => c.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+        }
+
+    }
+
+    internal class CreateProductCommandHandler(IDocumentSession documentSession, IValidator<CreateProductCommand> validator) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         private readonly IDocumentSession _documentSession = documentSession;
-       
+        private readonly IValidator<CreateProductCommand> _validator = validator;
+
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
+            var result = await _validator.ValidateAsync(command, cancellationToken);
+
+            var errors = result.Errors.Select(e => e.ErrorMessage).ToList();
+
+            if (errors.Count != 0) 
+            {
+                throw new ValidationException(errors.FirstOrDefault());
+            }
+
             Product product = new()
             {
                 Name = command.Name,
