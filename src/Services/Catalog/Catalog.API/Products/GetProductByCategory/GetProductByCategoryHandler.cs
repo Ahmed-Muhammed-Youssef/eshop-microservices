4b2f95067343d581e598a40ad971d6a60d33d243
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-12-14 18:33:21 +0200

    feat: add the global logging instead of the local logging #3

--- src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -2,15 +2,12 @@
 {
     public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
     public record GetProductByCategoryResult(IEnumerable<Product> Products);
-    internal class GetProductByCategoryHandler(IDocumentSession doumentSession, ILogger<GetProductByCategoryHandler> logger) : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+    internal class GetProductByCategoryHandler(IDocumentSession doumentSession) : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
     {
         private readonly IDocumentSession _doumentSession = doumentSession;
-        private readonly ILogger<GetProductByCategoryHandler> _logger = logger;
 
         public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
         {
-            _logger.LogInformation(nameof(GetProductByCategoryHandler) + " is called with query {@Query}", query);
-
             IEnumerable<Product> products = await _doumentSession.Query<Product>().Where(p => p.Category.Contains(query.Category)).ToListAsync(token: cancellationToken);
 
             return new GetProductByCategoryResult(products);
--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -2,15 +2,12 @@
 {
     public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
     public record GetProductByIdResult(Product Product);
-    public class GetProductByIdHandler(IDocumentSession documentSession, ILogger<GetProductByIdHandler> logger) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    public class GetProductByIdHandler(IDocumentSession documentSession) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
     {
         private readonly IDocumentSession _documentSession = documentSession;
-        private readonly ILogger<GetProductByIdHandler> _logger = logger;
 
         public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
         {
-            _logger.LogInformation(nameof(GetProductByIdHandler) + " is called with query {@Query}", query);
-
             var product = await _documentSession.LoadAsync<Product>(query.Id, cancellationToken) ?? throw new ProductNotFoundException(query.Id);
 
             return new GetProductByIdResult(product);
