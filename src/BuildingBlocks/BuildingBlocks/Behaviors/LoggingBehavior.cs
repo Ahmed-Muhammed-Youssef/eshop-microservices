2406a98f0a85f86db08b286dcd8ea6b0b2596d38
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-12-14 18:32:40 +0200

    feat: implement logging behavior for mediator pipeline #3

---
+++ src/BuildingBlocks/BuildingBlocks/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace BuildingBlocks.Behaviors
+{
+    public class LoggingBehavior<TRequest, TResponse>(ILogger<LoggingBehavior<TRequest, TResponse>> logger) : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : notnull, IRequest<TResponse>
+        where TResponse : notnull
+    {
+        private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger = logger;
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("[STRAT] Handle request={Request} - response={Response} - request data = {RequestData}", typeof(TRequest).Name, typeof(TResponse).Name, request);
+
+            var timer = new Stopwatch();
+            
+            timer.Start();
+
+            var response = await next();
+
+            timer.Stop();
+
+            var timeTaken = timer.Elapsed;
+
+            if(timeTaken.Seconds > 3)
+            {
+                _logger.LogWarning("[PERFORMANCE] the request {Request} took {TimeTaken}", typeof(TRequest).Name, timeTaken.Seconds);
+            }
+
+            _logger.LogInformation("[END] Handled {Request} With {Response}", typeof(TRequest).Name, response);
+
+            return response;
+        }
+    }
+}
