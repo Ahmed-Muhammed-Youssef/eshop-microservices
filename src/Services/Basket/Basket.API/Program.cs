4d8ebd1acb963e6f355e55b7ab57836af09adbad
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-01-10 18:27:57 +0200

    feat: add carter library #6

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,9 +1,13 @@
+using Carter;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services 
+builder.Services.AddCarter();
 
 var app = builder.Build();
 
 // Configure pipeline
+app.MapCarter();
 
 app.Run();
