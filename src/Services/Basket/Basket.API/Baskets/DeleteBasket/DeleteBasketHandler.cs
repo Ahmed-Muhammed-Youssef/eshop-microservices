8c09331a5e7e52b260ef399139a68fdd32080df3
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-01-10 20:13:35 +0200

    feat: add delete basket endpoint #6

---
+++ src/Services/Basket/Basket.API/Baskets/DeleteBasket/DeleteBasketHandler.cs
@@ -0,0 +1,23 @@
+﻿using BuildingBlocks.CQRS;
+using FluentValidation;
+
+namespace Basket.API.Baskets.DeleteBasket
+{
+    public record DeleteBasketCommand(string Username) : ICommand<DeleteBasketResult>;
+    public record DeleteBasketResult(bool IsSuccess);
+
+    public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
+    {
+        public DeleteBasketCommandValidator()
+        {
+            RuleFor(c => c.Username).NotEmpty().WithMessage("Username is required");
+        }
+    }
+    public class DeleteBasketHandler : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+    {
+        public Task<DeleteBasketResult> Handle(DeleteBasketCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
