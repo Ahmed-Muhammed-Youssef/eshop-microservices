51946ba95e6de033006b685c0a5c2da7968e5645
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-01-10 20:15:46 +0200

    feat: add mediator to the di container #6

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,8 +1,18 @@
+using BuildingBlocks.Behaviors;
 using Carter;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services 
+var programAssemply = typeof(Program).Assembly;
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(programAssemply);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
 builder.Services.AddCarter();
 
 var app = builder.Build();
