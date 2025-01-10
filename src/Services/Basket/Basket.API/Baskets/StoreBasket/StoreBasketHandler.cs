6b25fc8f9a80043791890f3ea8ecab7f5657196e
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-01-10 20:13:20 +0200

    feat: add store basket endpoint #6

---
+++ src/Services/Basket/Basket.API/Baskets/StoreBasket/StoreBasketHandler.cs
@@ -0,0 +1,26 @@
+﻿using Basket.API.Models;
+using BuildingBlocks.CQRS;
+using FluentValidation;
+
+namespace Basket.API.Baskets.StoreBasket
+{
+    public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+    public record StoreBasketResult(string Username);
+
+    public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
+    {
+        public StoreBasketCommandValidator()
+        {
+            RuleFor(c => c.Cart).NotNull().WithMessage("Caer can not be null");
+            RuleFor(c => c.Cart.Username).NotEmpty().WithMessage("Username is required");
+        }
+    }
+
+    public class StoreBasketHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+    {
+        public Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
