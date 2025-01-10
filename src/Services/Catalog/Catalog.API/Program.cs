81918ef3fa2aed44a4bd3616103e0d7b04b41adf
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-01-10 15:13:44 +0200

    feat: add health checks for postgresql #3

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,5 +1,7 @@
 
 using Catalog.API.Data;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add Services
@@ -27,7 +29,8 @@ if (builder.Environment.IsDevelopment())
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
-builder.Services.AddHealthChecks();
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("PostgresConnection")!);
 
 var app = builder.Build();
 
@@ -36,6 +39,8 @@ app.UseExceptionHandler(options => { });
 
 app.MapCarter();
 
-app.UseHealthChecks("/health");
+app.UseHealthChecks("/health", new HealthCheckOptions() { 
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
