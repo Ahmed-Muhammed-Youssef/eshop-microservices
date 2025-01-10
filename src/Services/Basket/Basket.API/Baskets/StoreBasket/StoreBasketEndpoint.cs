d8188f4654436b6813fe3b8c99d39fe436f9a4b1
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-01-10 20:13:20 +0200

    feat: add store basket endpoint #6

---
+++ src/Services/Basket/Basket.API/Baskets/StoreBasket/StoreBasketEndpoint.cs
@@ -0,0 +1,29 @@
+﻿using Basket.API.Models;
+using Carter;
+using Mapster;
+using MediatR;
+
+namespace Basket.API.Baskets.StoreBasket
+{
+    public record StoreBasketRequest(ShoppingCart Cart);
+    public record StoreBasketResponse(string Username);
+    public class StoreBasketEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/basket", async (StoreBasketRequest request, ISender sender) => {
+
+                StoreBasketCommand command = request.Adapt<StoreBasketCommand>();
+
+                StoreBasketResult result = await sender.Send(command);
+
+                return Results.Ok(result);
+            })
+            .WithName("StoreBasket")
+            .Produces<StoreBasketResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Store basket")
+            .WithDescription("Store basket");
+        }
+    }
+}
