0d73d667e793997687fc0b7051c945213a4115c5
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-01-11 18:18:29 +0200

    feat: add decorator around the basket repository #8

---
+++ src/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace Basket.API.Data
+{
+    public class CachedBasketRepository(IBasketRepository basketRepository) : IBasketRepository
+    {
+        public async Task<ShoppingCart> GetBasket(string username, CancellationToken cancellationToken = default)
+        {
+            return await basketRepository.GetBasket(username, cancellationToken);
+        }
+        public async Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+        {
+            return await basketRepository.StoreBasket(basket, cancellationToken);
+        }
+        public async Task<bool> DeleteBasket(string username, CancellationToken cancellationToken = default)
+        {
+            return await basketRepository.DeleteBasket(username, cancellationToken);
+        }
+    }
+}
--- src/Services/Basket/Basket.API/GlobalUsings.cs
+++ src/Services/Basket/Basket.API/GlobalUsings.cs
@@ -8,6 +8,7 @@ global using FluentValidation;
 global using BuildingBlocks.Behaviors;
 global using BuildingBlocks.CQRS;
 global using BuildingBlocks.Exceptions;
+global using BuildingBlocks.Exceptions.Handler;
 
 global using Basket.API.Models;
 global using Basket.API.Data;
