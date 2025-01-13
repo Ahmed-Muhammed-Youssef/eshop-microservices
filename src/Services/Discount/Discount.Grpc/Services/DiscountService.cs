3687968a86b933f39cc8b31c10f5601beb8f5982
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-01-13 22:45:27 +0200

    feat: add empty discount service #10

---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Core;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService : Discount.DiscountBase
+    {
+        public override Task<CouponModel> GetDiscount(ProductKey request, ServerCallContext context)
+        {
+            return base.GetDiscount(request, context);
+        }
+        public override Task<CouponModel> CreateDiscount(CouponModel request, ServerCallContext context)
+        {
+            return base.CreateDiscount(request, context);
+        }
+        public override Task<CouponModel> UpdateDiscount(CouponModel request, ServerCallContext context)
+        {
+            return base.UpdateDiscount(request, context);
+        }
+        public override Task<DeleteDiscountResponse> DeleteDiscount(ProductKey request, ServerCallContext context)
+        {
+            return base.DeleteDiscount(request, context);
+        }
+    }
+}
