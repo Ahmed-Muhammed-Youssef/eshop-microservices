ee3930b818b3cd10bf2dd2c0530f1a69b6a25a24
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-01-14 22:22:01 +0200

    feat: implement get discount method #10

--- src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -1,12 +1,25 @@
-﻿using Grpc.Core;
+﻿using Discount.Grpc.Data;
+using Discount.Grpc.Models;
+using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
 
 namespace Discount.Grpc.Services
 {
-    public class DiscountService : Discount.DiscountBase
+    public class DiscountService(DiscountContext dbContext, ILogger<DiscountService> logger) : Discount.DiscountBase
     {
-        public override Task<CouponModel> GetDiscount(ProductKey request, ServerCallContext context)
+        public override async Task<CouponModel> GetDiscount(ProductKey request, ServerCallContext context)
         {
-            return base.GetDiscount(request, context);
+            Coupon? coupon = await dbContext.Coupons.Where(c => c.ProductName == request.ProductName).FirstOrDefaultAsync()
+                ?? new Coupon() { ProductName="No Discount", Amount = 0, Description = "No Discount"};
+            logger.LogInformation("Discount is retrieved for ProductName {productName}", coupon.ProductName);
+
+            CouponModel response = new() { 
+                Id = coupon.Id,
+                ProductName = coupon.ProductName,
+                Amount = coupon.Amount,
+                Description = coupon.Description 
+            };
+            return response;
         }
         public override Task<CouponModel> CreateDiscount(CouponModel request, ServerCallContext context)
         {
