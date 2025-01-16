5740cbe8ce841a90fa26195fb150f8e3ace13144
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-01-16 18:09:52 +0200

    feat: add dependency injection services for each layer #14

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,5 +1,13 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
 // Add Services
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddApiServices();
 
 var app = builder.Build();
 // Configure Pipeline
