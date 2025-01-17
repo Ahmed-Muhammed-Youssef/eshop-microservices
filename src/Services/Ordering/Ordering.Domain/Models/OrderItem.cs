0dad9eb430d3ad628a0b7136956675a9303df019
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-01-17 21:02:15 +0200

    feat: add a builder constructor for order item entity #14

--- src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -2,6 +2,14 @@
 {
     public class OrderItem : Entity<Guid>
     {
+        public OrderItem(Guid orderId, Guid productId, decimal price, int quantity)
+        {
+            Id = Guid.NewGuid();
+            OrderId = orderId;
+            ProductId = productId;
+            Price = price;
+            Quantity = quantity;
+        }
         public Guid OrderId { get; private set; }
         public Guid ProductId { get; private set; }
         public decimal Price { get; private set; }
