58f71662ce188fc8c94793914536221f58e4ceb7
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-01-17 16:48:20 +0200

    feat: add abstraction classes #14

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Aggregate.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Ordering.Domain.Abstractions
+{
+    public abstract class Aggregate<TID> : Entity<TID>, IAggregate<TID>
+    {
+        private readonly List<IDomainEvent> _domainEvents = new();
+        public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+        public IDomainEvent[] ClearDomainEvents()
+        {
+            IDomainEvent[] resultDomainEvents = [.. _domainEvents];
+
+            _domainEvents.Clear();
+
+            return resultDomainEvents;
+        }
+
+        public void AddDominEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IAggregate.cs
@@ -0,0 +1,12 @@
+﻿namespace Ordering.Domain.Abstractions
+{
+    public interface IAggregate<T> : IEntiy<T>, IAggregate
+    {
+
+    }
+    public interface IAggregate : IEntity
+    {
+        public IReadOnlyList<IDomainEvent> DomainEvents { get; }
+        public IDomainEvent[] ClearDomainEvents();
+    }
+}
