03cc0b441236b3ac62bcd5eb46f911332c1131e2
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-01-17 17:48:15 +0200

    feat: add domain entities #14

---
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -0,0 +1,12 @@
+﻿using Ordering.Domain.Abstractions;
+
+namespace Ordering.Domain.Models
+{
+    public class OrderItem : Entity<Guid>
+    {
+        public Guid OrderId { get; private set; }
+        public Guid ProductId { get; private set; }
+        public decimal Price { get; private set; }
+        public int Quantity { get; private set; }
+    }
+}
