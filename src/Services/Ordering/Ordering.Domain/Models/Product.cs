4d6cc9920ff48c5b966bab168cd480c9ee0cf058
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-01-17 21:57:56 +0200

    feat: add product static factory method #14

--- src/Services/Ordering/Ordering.Domain/Models/Product.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Product.cs
@@ -4,5 +4,14 @@
     {
         public string Name { get; private set; } = default!;
         public decimal Price { get; private set; }  
+
+        public static Product Create(Guid id, string name, decimal price)
+        {
+            ArgumentNullException.ThrowIfNull(id);
+            ArgumentException.ThrowIfNullOrEmpty(name);
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+            return new Product { Id = id, Name = name, Price = price };
+        }
     }
 }
