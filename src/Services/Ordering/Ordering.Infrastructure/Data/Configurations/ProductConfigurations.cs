f34b0cf7a765393c1b91ba8ae388ba794556935f
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-01-19 21:54:02 +0200

    feat: add database tables configurations #14

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/ProductConfigurations.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Models;
+
+namespace Ordering.Infrastructure.Data.Configurations
+{
+    public class ProductConfigurations : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+        }
+    }
+}
