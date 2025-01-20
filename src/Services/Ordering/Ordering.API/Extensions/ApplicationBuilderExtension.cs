13422eaed94c070a1861073a5abdc7e14192f008
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-01-20 23:43:15 +0200

    feat: add auto migration #14

---
+++ src/Services/Ordering/Ordering.API/Extensions/ApplicationBuilderExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Infrastructure.Data;
+
+namespace Ordering.API.Extensions
+{
+    public static class ApplicationBuilderExtension
+    {
+        public static IApplicationBuilder UseMigration(this IApplicationBuilder applicationBuilder)
+        {
+            using var scope = applicationBuilder.ApplicationServices.CreateScope();
+            using var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            dbContext.Database.MigrateAsync();
+
+            return applicationBuilder;
+        }
+    }
+}
