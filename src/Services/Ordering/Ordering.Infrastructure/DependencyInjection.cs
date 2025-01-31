9eda9a9d495f657373ae803a39380b28f6853079
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-01-31 14:28:02 +0200

    feat: add domain events dispatcher interceptor #14

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Ordering.Infrastructure.Data.Interceptors;
 
@@ -8,11 +9,15 @@ namespace Ordering.Infrastructure
     {
         public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
+            services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+            services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+            services.AddDbContext<ApplicationDbContext>((sp, options) =>
             {
-                options.AddInterceptors( new AuditableEntityInterceptor());
+                options.AddInterceptors( sp.GetServices<ISaveChangesInterceptor>());
                 options.UseSqlServer(configuration.GetConnectionString("SqlServer"));
             });
+
             return services;
         }
     }
