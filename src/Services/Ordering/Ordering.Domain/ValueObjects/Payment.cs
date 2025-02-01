df57af22e7ab5aeeb44e4a5d0e3f2a729c64b91e
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-02-01 13:49:49 +0200

    reafct: cvv name #14

--- src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
@@ -2,29 +2,29 @@
 {
     public record Payment
     {
-        private Payment(string? cardName, string cardNumber, string expiration, string cVV, int paymentMethod)
+        private Payment(string? cardName, string cardNumber, string expiration, string cvv, int paymentMethod)
         {
             CardName = cardName;
             CardNumber = cardNumber;
             Expiration = expiration;
-            CVV = cVV;
+            Cvv = cvv;
             PaymentMethod = paymentMethod;
         }
 
         public string? CardName { get; } = default!;
         public string CardNumber { get; } = default!;
         public string Expiration { get; } = default!;
-        public string CVV { get; } = default!;
+        public string Cvv { get; } = default!;
         public int PaymentMethod { get; }
 
-        public static Payment Of(string? cardName, string cardNumber, string expiration, string cVV, int paymentMethod)
+        public static Payment Of(string? cardName, string cardNumber, string expiration, string cvv, int paymentMethod)
         {
             ArgumentException.ThrowIfNullOrWhiteSpace(cardName, nameof(cardName));
             ArgumentException.ThrowIfNullOrWhiteSpace(expiration, nameof(expiration));
-            ArgumentException.ThrowIfNullOrWhiteSpace(cVV, nameof(cVV));
-            ArgumentOutOfRangeException.ThrowIfGreaterThan(cVV.Length, 3);
+            ArgumentException.ThrowIfNullOrWhiteSpace(cvv, nameof(cvv));
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(cvv.Length, 3);
 
-            return new Payment(cardName, cardNumber, expiration, cVV, paymentMethod);
+            return new Payment(cardName, cardNumber, expiration, cvv, paymentMethod);
         }
     }
 }
