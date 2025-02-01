66fde635fdbad14ab4ebb23d5e5e99edbdb81521
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-02-01 15:04:57 +0200

    feat: add update order command handler #14

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/UpdateOrder/UpdateOrderCommand.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Orders.Commands.UpdateOrder
+{
+    public record UpdateOrderCommand(OrderDto Order) : ICommand<UpdateOrderCommandResult>;
+    public record UpdateOrderCommandResult(bool IsSuccess);
+
+    public class UpdateOrderCommandValidator : AbstractValidator<UpdateOrderCommand>
+    {
+        public UpdateOrderCommandValidator()
+        {
+            RuleFor(o => o.Order.OrderName).NotEmpty().WithMessage("Name is required");
+            RuleFor(o => o.Order.CustomerId).NotNull().WithMessage("CustomerId is required");
+            RuleFor(o => o.Order.OrderItems).NotEmpty().WithMessage("OrderItems should not be empty");
+        }
+    }
+}
