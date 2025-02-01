43cdfc3b7d4f65aabce994c5459a75f30c910ac6
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-02-01 14:23:29 +0200

    feat: add interface for dbcontext #14

--- src/Services/Ordering/Ordering.Infrastructure/GlobalUsings.cs
+++ src/Services/Ordering/Ordering.Infrastructure/GlobalUsings.cs
@@ -1,6 +1,10 @@
 ﻿global using Microsoft.EntityFrameworkCore;
 
-global using Ordering.Domain.Abstractions;
 global using Ordering.Infrastructure.Data;
+global using Ordering.Infrastructure.Data.Interceptors;
+
+global using Ordering.Domain.Abstractions;
 global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
+
+global using Ordering.Application.Data;
