8d69451e19877f2d67e8bafec486278763b3d277
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-02-10 22:57:14 +0200

    feat: add get order by customer #14

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomer/GetOrderByCustomerHandler.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Extensions;
+
+namespace Ordering.Application.Orders.Queries.GetOrdersByCustomer
+{
+    public class GetOrderByCustomerHandler(IApplicationDbContext dbContext) : IQueryHandler<GetOrdersByCustomerQuery, GetOrderByCustomerQueryResult>
+    {
+        public async Task<GetOrderByCustomerQueryResult> Handle(GetOrdersByCustomerQuery request, CancellationToken cancellationToken)
+        {
+            List<OrderDto> orders = await dbContext.Orders
+                .Include(o => o.OrderItems)
+                .AsNoTracking()
+                .Where(o => o.CustomerId == request.CustomerId)
+                .Select(o => o.ToOrderDto())
+                .OrderBy(o => o.OrderName)
+                .ToListAsync(cancellationToken);
+
+            return new GetOrderByCustomerQueryResult(orders);
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomer/GetOrderByCustomerQuery.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Orders.Queries.GetOrdersByCustomer
+{
+    public record GetOrdersByCustomerQuery(Guid CustomerId) : IQuery<GetOrderByCustomerQueryResult>;
+    public record GetOrderByCustomerQueryResult(IEnumerable<OrderDto> Orders);
+    public class GetOrderByCustomerValidator : AbstractValidator<GetOrdersByCustomerQuery>
+    {
+        public GetOrderByCustomerValidator()
+        {
+            RuleFor(o => o.CustomerId).NotEmpty().WithMessage("CustomerId is required");
+        }
+    }
+}
