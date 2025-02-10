a84e05772afea687ad46bca99c5ac08e849a612c
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-02-10 22:44:40 +0200

    refact: move mapping logic to an extension method #14

--- src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrderByNameHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrderByNameHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Extensions;
 
 namespace Ordering.Application.Orders.Queries.GetOrdersByName
 {
@@ -10,20 +11,11 @@ namespace Ordering.Application.Orders.Queries.GetOrdersByName
                 .Include(o => o.OrderItems)
                 .AsNoTracking()
                 .Where(o => o.OrderName.Contains(request.Name))
-                .Select(o => ProjectToOrderDto(o))
+                .Select(o => o.ToOrderDto())
                 .OrderBy(o => o.OrderName)
                 .ToListAsync(cancellationToken);
 
             return new GetOrderByNameQueryResult(orders);
-        }
-
-        private OrderDto ProjectToOrderDto(Order order)
-        {
-            AddressDto billingAddressDto = new(order.BillingAddress.FirstName, order.BillingAddress.LastName, order.BillingAddress.EmailAddress, order.BillingAddress.AddressLine, order.BillingAddress.Country, order.BillingAddress.State, order.BillingAddress.ZipCode);
-            AddressDto shippingAddressDto = new(order.ShippingAddress.FirstName, order.ShippingAddress.LastName, order.ShippingAddress.EmailAddress, order.ShippingAddress.AddressLine, order.ShippingAddress.Country, order.ShippingAddress.State, order.ShippingAddress.ZipCode);
-            PaymentDto paymentDto = new(order.Payment.CardName, order.Payment.CardNumber, order.Payment.Expiration, order.Payment.Cvv, order.Payment.PaymentMethod);
-            List<OrderItemDto> orderItems = order.OrderItems.Select(oi => new OrderItemDto(oi.OrderId, oi.ProductId, oi.Quantity, oi.Price)).ToList();
-            return new(order.Id, order.CustomerId, order.OrderName, shippingAddressDto, billingAddressDto, paymentDto, order.Status, orderItems);
         }
     }
 }
