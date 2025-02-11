d391084ff9d26871d1f3819603b4a5ec9af708c4
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-02-11 20:23:27 +0200

    feat: add get orders #14

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersHandler.cs
@@ -0,0 +1,28 @@
+﻿using BuildingBlocks.Pagination;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Extensions;
+
+namespace Ordering.Application.Orders.Queries.GetOrders
+{
+    public class GetOrdersHandler(IApplicationDbContext dbContext) : IQueryHandler<GetOrdersQuery, GetOrdersQueryResult>
+    {
+        public async Task<GetOrdersQueryResult> Handle(GetOrdersQuery request, CancellationToken cancellationToken)
+        {
+            long count = await dbContext.Orders
+               .AsNoTracking()
+               .LongCountAsync(cancellationToken);
+
+            List<OrderDto> orders = await dbContext.Orders
+               .Include(o => o.OrderItems)
+               .AsNoTracking()
+               .Select(o => o.ToOrderDto())
+               .Skip(request.PaginationRequest.PageIndex * request.PaginationRequest.PageSize)
+               .Take(request.PaginationRequest.PageSize)
+               .OrderBy(o => o.OrderName)
+               .ToListAsync(cancellationToken);
+
+            PaginatedResult<OrderDto> paginatedOrders = new(request.PaginationRequest.PageIndex, request.PaginationRequest.PageSize, count, orders);
+            return new GetOrdersQueryResult(paginatedOrders);
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersQuery.cs
@@ -0,0 +1,15 @@
+﻿using BuildingBlocks.Pagination;
+using FluentValidation;
+
+namespace Ordering.Application.Orders.Queries.GetOrders
+{
+    public record GetOrdersQueryResult(PaginatedResult<OrderDto> Orders);
+    public record GetOrdersQuery(PaginationRequest PaginationRequest) : IQuery<GetOrdersQueryResult>;
+    public class GetOrdersQueryValidator : AbstractValidator<GetOrdersQuery>
+    {
+        public GetOrdersQueryValidator()
+        {
+                
+        }
+    }
+}
