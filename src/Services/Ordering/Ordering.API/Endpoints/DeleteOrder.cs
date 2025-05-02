580f18bf87c61a9e3f9fc88f0978f10f2c879747
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-05-02 11:26:12 +0300

    feat: add delete order endpoint

---
+++ src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Ordering.API.Endpoints
+{
+    public record DeleteOrderResponse(bool IsSuccess);
+    public class DeleteOrder : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/orders/{id}", async (Guid id, ISender sender) =>
+            {
+                DeleteOrderCommand command = new(id);
+
+                DeleteOrderResult result = await sender.Send(command);
+                DeleteOrderResponse response = result.Adapt<DeleteOrderResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("DeleteOrder")
+            .Produces<DeleteOrderResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Delete Order")
+            .WithDescription("Delete Order"); ;
+        }
+    }
+}
