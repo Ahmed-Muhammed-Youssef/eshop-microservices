86adcd0438d0f0e325d6ebcd323294e277525ab0
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-05-02 11:34:30 +0300

    feat: add get orders by name endpoint

---
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByName.cs
@@ -0,0 +1,24 @@
+﻿using Ordering.Application.Orders.Queries.GetOrdersByName;
+
+namespace Ordering.API.Endpoints
+{
+    public record GetOrdersByNameResponse(IEnumerable<OrderDto> Orders);
+    public class GetOrdersByName : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/orders/{name}", async(string name, ISender sender) =>
+            {
+                GetOrderByNameQueryResult result = await sender.Send(new GetOrderByNameQuery(name));
+
+                return Results.Ok(result.Adapt<GetOrdersByNameResponse>());
+            })
+            .WithName("GetOrdersByName")
+            .Produces<GetOrdersByNameResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Get Order By Name")
+            .WithDescription("Get Order By Name");
+        }
+    }
+}
