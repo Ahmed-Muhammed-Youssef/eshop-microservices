2021915c3ae732d0c92062d7e6220eb4c6f0b91e
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-05-02 11:19:39 +0300

    feat: add update order endpoint

--- src/Services/Ordering/Ordering.API/GlobalUsings.cs
+++ src/Services/Ordering/Ordering.API/GlobalUsings.cs
@@ -1,5 +1,8 @@
 ﻿global using Ordering.API;
+
 global using Ordering.Application;
+global using Ordering.Application.Dtos;
+
 global using Ordering.Infrastructure;
 global using Ordering.Infrastructure.Data.Extensions;
 
