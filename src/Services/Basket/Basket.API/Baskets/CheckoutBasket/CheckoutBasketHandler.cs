7e8de2fe84b4a7e8eb3e69e17fda73811ccc138a
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-05-03 16:59:29 +0300

    feat: add basket checkout handler #16

---
+++ src/Services/Basket/Basket.API/Baskets/CheckoutBasket/CheckoutBasketHandler.cs
@@ -0,0 +1,36 @@
+﻿using Basket.API.Dtos;
+using BuildingBlocks.Messaging.Events;
+using MassTransit;
+
+namespace Basket.API.Baskets.CheckoutBasket
+{
+    public record CheckoutBasketCommand(BasketCheckoutDto Basket) : ICommand<CheckoutBasketResult>;
+    public record CheckoutBasketResult(bool IsSuccess);
+    public class CheckoutBasketCommandValidator : AbstractValidator<CheckoutBasketCommand>
+    {
+        public CheckoutBasketCommandValidator()
+        {
+            RuleFor(c => c.Basket).NotNull().WithMessage("Basket can not be null");
+            RuleFor(c => c.Basket.UserName).NotEmpty().WithMessage("UserName is required");
+        }
+    }
+    public class CheckoutBasketHandler(IBasketRepository repository, IPublishEndpoint publishEndpoint) : ICommandHandler<CheckoutBasketCommand, CheckoutBasketResult>
+    {
+        public async Task<CheckoutBasketResult> Handle(CheckoutBasketCommand command, CancellationToken cancellationToken)
+        {
+            var basket = await repository.GetBasket(command.Basket.UserName, cancellationToken);
+            if (basket == null)
+            {
+                return new CheckoutBasketResult(false);
+            }
+            BasketCheckoutEvent eventMessage = command.Basket.Adapt<BasketCheckoutEvent>();
+            eventMessage.TotalPrice = basket.TotalPrice;
+
+            await publishEndpoint.Publish(eventMessage, cancellationToken);
+
+            await repository.DeleteBasket(command.Basket.UserName, cancellationToken);
+
+            return new CheckoutBasketResult(true);
+        }
+    }
+}
