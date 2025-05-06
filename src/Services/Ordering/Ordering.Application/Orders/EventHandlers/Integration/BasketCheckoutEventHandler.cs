c6d33eb8dda73f5615473802cd0ce8ec6054090d
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-05-06 22:44:29 +0300

    feat: add checkout integration event handler #16

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Integration/BasketCheckoutEventHandler.cs
@@ -0,0 +1,42 @@
+﻿using BuildingBlocks.Messaging.Events;
+using MassTransit;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Orders.Commands.CreateOrder;
+
+namespace Ordering.Application.Orders.EventHandlers.Integration
+{
+    public class BasketCheckoutEventHandler(ISender sender, ILogger<BasketCheckoutEventHandler> logger) : IConsumer<BasketCheckoutEvent>
+    {
+        public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+        {
+            logger.LogInformation("Integration event handled: {IntegrationEvent}", context.Message.GetType().Name);
+            var command = MapToCreateOrderCommand(context.Message);
+
+            var result = await sender.Send(command, context.CancellationToken);
+        }
+        private static CreateOrderCommand MapToCreateOrderCommand(BasketCheckoutEvent message)
+        {
+            var addressDto = new AddressDto(message.FirstName, message.LastName, message.EmailAddress, message.AddressLine, message.Country, message.State, message.ZipCode);
+            var paymentDto = new PaymentDto(message.CardName, message.CardNumber, message.Expiration, message.Cvv, message.PaymentMethod);
+            var orderId = Guid.NewGuid();
+
+            // note: fixed values for simplicity
+            // in a real-world application, you would use the message values
+            var orderDto = new OrderDto(
+                Id: orderId,
+                CustomerId: message.CustomerId,
+                OrderName: message.UserName,
+                ShippingAddress: addressDto,
+                BillingAddress: addressDto,
+                Payment: paymentDto,
+                Status: Ordering.Domain.Enums.OrderStatus.Pending,
+                OrderItems:
+                [
+                    new OrderItemDto(orderId, Guid.NewGuid(), 2, 500),
+                    new OrderItemDto(orderId, Guid.NewGuid(), 1, 400)
+                ]);
+            return new CreateOrderCommand(orderDto);
+        }
+    }
+}
