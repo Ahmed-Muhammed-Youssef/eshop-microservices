3e4e9daa7d75fd9de118e216be8b1f0976f6a6f6
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-05-06 22:53:03 +0300

    feat: raise an integration event when order is created #16

--- src/Services/Ordering/Ordering.Application/Extensions/OrderExtensions.cs
+++ src/Services/Ordering/Ordering.Application/Extensions/OrderExtensions.cs
@@ -4,9 +4,9 @@
     {
         public static OrderDto ToOrderDto(this Order order) 
         {
-            AddressDto billingAddressDto = new(order.BillingAddress.FirstName, order.BillingAddress.LastName, order.BillingAddress.EmailAddress, order.BillingAddress.AddressLine, order.BillingAddress.Country, order.BillingAddress.State, order.BillingAddress.ZipCode);
-            AddressDto shippingAddressDto = new(order.ShippingAddress.FirstName, order.ShippingAddress.LastName, order.ShippingAddress.EmailAddress, order.ShippingAddress.AddressLine, order.ShippingAddress.Country, order.ShippingAddress.State, order.ShippingAddress.ZipCode);
-            PaymentDto paymentDto = new(order.Payment.CardName, order.Payment.CardNumber, order.Payment.Expiration, order.Payment.Cvv, order.Payment.PaymentMethod);
+            AddressDto billingAddressDto = new(order.BillingAddress.FirstName, order.BillingAddress.LastName, order.BillingAddress.EmailAddress ?? "", order.BillingAddress.AddressLine, order.BillingAddress.Country, order.BillingAddress.State, order.BillingAddress.ZipCode);
+            AddressDto shippingAddressDto = new(order.ShippingAddress.FirstName, order.ShippingAddress.LastName, order.ShippingAddress.EmailAddress ?? "", order.ShippingAddress.AddressLine, order.ShippingAddress.Country, order.ShippingAddress.State, order.ShippingAddress.ZipCode);
+            PaymentDto paymentDto = new(order.Payment.CardName ?? "", order.Payment.CardNumber, order.Payment.Expiration, order.Payment.Cvv, order.Payment.PaymentMethod);
             List<OrderItemDto> orderItems = order.OrderItems.Select(oi => new OrderItemDto(oi.OrderId, oi.ProductId, oi.Quantity, oi.Price)).ToList();
             return new(order.Id, order.CustomerId, order.OrderName, shippingAddressDto, billingAddressDto, paymentDto, order.Status, orderItems);
         }
