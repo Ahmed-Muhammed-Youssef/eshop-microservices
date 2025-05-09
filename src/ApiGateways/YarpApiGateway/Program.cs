bb869b9c0840ed2d93df777bfaf4bd6e383dcb10
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-05-09 13:54:13 +0300

    feat: add yarp configs #18

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,9 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
 
 // Configure pipline
+app.MapReverseProxy();
 
 app.Run();
