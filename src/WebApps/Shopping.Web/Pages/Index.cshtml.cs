855e72a347ad215e2616dab347d50cba3aa28cac
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-05-10 16:54:05 +0300

    feat: add cart redirection in index page #20

--- src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
+++ src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
@@ -1,26 +1,45 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Shopping.Web.Models.Basket;
 using Shopping.Web.Models.Catalog;
 using Shopping.Web.Services;
 
 namespace Shopping.Web.Pages
 {
-    public class IndexModel(ILogger<IndexModel> logger, ICatalogService catalogService) : PageModel
+    public class IndexModel(ILogger<IndexModel> logger, ICatalogService catalogService, IBasketService basketService) : PageModel
     {
         public IEnumerable<ProductModel> ProductList { get; set; } = [];
 
-        public async Task OnGetAsync()
+        public async Task<IActionResult> OnGetAsync()
         {
             logger.LogInformation("Index page loading...");
-            try
-            {
-                var result = await catalogService.GetProducts();
-                ProductList = result.Products;
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex, "Error fetching products.");
-            }
+            
+            var result = await catalogService.GetProducts();
+            ProductList = result.Products;
+            return Page();
         }
+
+        public async Task<IActionResult> OnPostAsync(Guid productId)
+        {
+            logger.LogInformation("Adding product to basket...");
+            
+            var productResponse = await catalogService.GetProduct(productId);
+
+            var basket = await basketService.LoadUserBasket();
+
+            basket.Items.Add(new ShoppingCartItemModel
+            {
+                ProductId = productId,
+                ProductName = productResponse.Product.Name,
+                Price = productResponse.Product.Price,
+                Quantity = 1,
+                Color = "Black"
+            });
+
+            await basketService.StoreBasket(new StoreBasketRequest(basket));
+
+            return RedirectToPage("Cart");
+        }
+
     }
 }
