c017510d0ae40ca97340c29cadcdd1d3a134befb
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-05-10 11:48:24 +0300

    feat: add services models #20

---
+++ src/WebApps/Shopping.Web/Models/Basket/BasketCheckoutModel.cs
@@ -0,0 +1,26 @@
+﻿namespace Shopping.Web.Models.Basket
+{
+    public class BasketCheckoutModel
+    {
+        public string UserName { get; set; } = string.Empty;
+        public Guid CustomerId { get; set; }
+        public decimal TotalPrice { get; set; }
+        // Shipping and Billing Address
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string EmailAddress { get; set; } = string.Empty;
+        public string AddressLine { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string ZipCode { get; set; } = string.Empty;
+        // Payment
+        public string CardName { get; set; } = string.Empty;
+        public string Cvv { get; set; } = string.Empty;
+        public string CardNumber { get; set; } = string.Empty;
+        public string Expiration { get; set; } = string.Empty;
+        public int PaymentMethod { get; set; }
+    }
+
+    public record CheckoutBasketRequest(BasketCheckoutModel Basket);
+    public record CheckoutBasketResponse(bool IsSuccess);
+}
