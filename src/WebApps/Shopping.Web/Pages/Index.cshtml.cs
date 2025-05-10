fbb3227c7c88c55a9e352eba68f57ef96de31c5d
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-05-10 16:27:23 +0300

    feat: add index page #20

--- src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
+++ src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
@@ -1,20 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Shopping.Web.Models.Catalog;
+using Shopping.Web.Services;
 
 namespace Shopping.Web.Pages
 {
-    public class IndexModel : PageModel
+    public class IndexModel(ILogger<IndexModel> logger, ICatalogService catalogService) : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        public IEnumerable<ProductModel> ProductList { get; set; } = [];
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public async Task OnGetAsync()
         {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-
+            logger.LogInformation("Index page loading...");
+            try
+            {
+                var result = await catalogService.GetProducts();
+                ProductList = result.Products;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error fetching products.");
+            }
         }
     }
 }
