f8bae4da6464689c6ab43a4ca329ebcd717c9f87
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2025-05-10 11:48:24 +0300

    feat: add services models #20

---
+++ src/WebApps/Shopping.Web/Models/Basket/ShoppingCartModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Shopping.Web.Models.Basket
+{
+    public class ShoppingCartModel
+    {
+        public string Username { get; set; } = default!;
+        public List<ShoppingCartItemModel> Items { get; set; } = [];
+        public decimal TotalPrice => Items.Sum(i => i.Price * i.Quantity);
+    }
+
+    public record GetBasketResponse(ShoppingCartModel Cart);
+    public record StoreBasketRequest(ShoppingCartModel Cart);
+    public record StoreBasketResponse(string Username);
+    public record DeleteBasketResponse(bool IsSuccess);
+}
